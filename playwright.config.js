41dffc271a32f08f595f5c0b0ab0a56bc5a922a6
Alex <sashaa1986@rambler.ru>
2025-09-09 14:12:20 +0300

    Add docker

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
+  //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -35,30 +36,30 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
+    /* {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+      use: { ...devices['Desktop Chrome'],  channel: 'chromium' },
+    }, */
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+    /* {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    }, */
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+   /*  {
+       name: 'Mobile Chrome',
+       use: { ...devices['Pixel 5'] },
+    }, */
+   /*  {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    }, */
 
     /* Test against branded browsers. */
     // {
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,39 +1,69 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+
 test('search', async ({ page }) => {
+  await test.step('Open page', async () => {
+    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/');
+  });
   
-  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/');
 
-  // Set search input value "ro" 
-  await page.fill('input.search-keyword', 'ro');
+  // Set search input value "ro" s
+  await test.step('Search for "ro"', async () => {
+    await page.fill('input.search-keyword', 'ro');
+    await page.waitForTimeout(2000);
+  });  
 
-  //  Find a card with carrots
-  const carrotCard = page.locator('.product').filter({hasText: 'Carrot - 1 Kg'});
-  // Set quantity to 3
-  await carrotCard.locator('.quantity').fill('3');
+  //  Find a card with carrots and set quantity to 5
+  let carrotCard = null;
+  await test.step('Find a card with carrots and set quantity to 5', async () => {
+    carrotCard = page.locator('.product').filter({hasText: 'Carrot - 1 Kg'});
+    await carrotCard.locator('.quantity').fill('5');
+    await page.waitForTimeout(2000);
+  });
   
-  // Find a card with mushrooms
-  const mushroomCard = page.locator('.product').filter({hasText: 'Mushroom - 1 Kg'});
   
-  //Set the number to three using the increment button
-  const qtyClick = 3; 
-  for(let i=1; i<qtyClick; i++) {
-    await mushroomCard.locator('a.increment').click();    
-  }
+  
+  // Find a card with mushrooms and set the number to three using the increment button
+  let mushroomCard = null;
+  await test.step('Find a card with mushrooms and set the number to three using the increment button', async () => {
+    mushroomCard = page.locator('.product').filter({hasText: 'Mushroom - 1 Kg'});
+    
+    const qtyClick = 3; 
+    for(let i=1; i<qtyClick; i++) {
+      await mushroomCard.locator('a.increment').click();    
+    }
+
+    await page.waitForTimeout(2000);
+  });
+
+  
   
   //Add to cart carrot
-  await carrotCard.locator('button').click(); 
+  await test.step('Add to cart carrot', async () => {
+    await carrotCard.locator('button').click();
+    await page.waitForTimeout(2000);
+  });
+   
 
   //Add to cart mushroom
-  await mushroomCard.locator('button').click();  
+  await test.step('Add to cart mushroom', async () => {
+      await mushroomCard.locator('button').click(); 
+      await page.waitForTimeout(2000); 
+  });
+
 
   //Open cart
-  await page.locator('a.cart-icon').click(); 
-
-  //Delete carrot from the cart     
-  const carrotItem = page.locator('ul.cart-items>li').filter({hasText: 'Carrot - 1 Kg'}).first();  
-  await carrotItem.locator('a.product-remove').click();  
-
-  await page.pause();
+  await test.step('Open cart', async () => {
+    await page.locator('a.cart-icon').click(); 
+    await page.waitForTimeout(2000);
+  });
+  
+  //Delete carrot from the cart 
+  await test.step('Delete carrot from the cart', async () => {
+    const carrotItem = page.locator('ul.cart-items>li').filter({hasText: 'Carrot - 1 Kg'}).first();  
+    await carrotItem.locator('a.product-remove').click();  
+    await page.waitForTimeout(2000);
+  });    
+ 
 });
